9d541f2a92e4b7816d3058b6dd4c9f29af0fe1a9
Rashad J Reed <reedrichards04@gmail.com>
2018-08-09 02:15:55 -0400

    Added Sub Menus to main menu bar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+  $('.menu').click(function(){
+    $(this).siblings().removeClass('active-sub-menu');
+    $('ul').toggleClass('active-sub-menu')
+  })
+  $('ul li').click(function(){
+    $(this).siblings().removeClass('active');
+    $(this).toggleClass('active');
+  })
+})
