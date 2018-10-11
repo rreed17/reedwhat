b31e79b23d5fae7ac36ab564ba7be7209bf35c31
Rashad J Reed <reedrichards04@gmail.com>
2018-10-10 23:28:42 -0400

    Added scss to Website

--- js/main.js
+++ js/main.js
@@ -19,6 +19,12 @@ const imgsM = document.querySelectorAll('.top-covers-imgs-m img');
 //Top 50 Covers Right Gallery VARIABLES
 const currentR = document.querySelector('#TopCoversCurrentImgR');
 const imgsR = document.querySelectorAll('.top-covers-imgs-r img');
+//Top 50 Covers 2nd row Left Gallery VARIABLES
+const current2L = document.querySelector('#TopCoversCurrentImg2l');
+const imgs2L = document.querySelectorAll('.top-covers-imgs-2l img');
+//Top 50 Covers 2nd row Right Gallery VARIABLES
+const current2R = document.querySelector('#TopCoversCurrentImg2r');
+const imgs2R = document.querySelectorAll('.top-covers-imgs-2r img');
 
 const opacity = 0.4;
 
@@ -28,6 +34,10 @@ imgsM.forEach(img => img.addEventListener('click',
 imgClickMid));
 imgsR.forEach(img => img.addEventListener('click',
 imgClickRight));
+imgs2L.forEach(img => img.addEventListener('click',
+imgClick2Left));
+imgs2R.forEach(img => img.addEventListener('click',
+imgClick2Right));
 
 
 function imgClick(e){
@@ -65,6 +75,20 @@ function imgClickRight(e){
   currentR.classList.add('top-covers-fade-in');
   //Remove tcFadeIn animation class
   setTimeout(() => currentR.classList.remove('top-covers-fade-in'),500);
+//Change the opacity to opacity Variable
+  e.target.style.opacity = opacity;
+}
+
+//2 row Right Gallery function for selecting image
+function imgClick2Right(e){
+  //Reset the opacity
+  imgs2R.forEach(img => (img.style.opacity =1));
+  //change current left image to src of clicked left ten covers
+  current2R.src = e.target.src;
+  //Add tcFadeIn animation class to selected current Left covers
+  current2R.classList.add('top-covers-fade-in');
+  //Remove tcFadeIn animation class
+  setTimeout(() => current2R.classList.remove('top-covers-fade-in'),500);
 //Change the opacity to opacity Variable
   e.target.style.opacity = opacity;
 }
